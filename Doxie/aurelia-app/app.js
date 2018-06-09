790997b3b34db47fb197b1c910b37c1ee78f9a03
vnmatt <gordon_matt@hotmail.com>
2018-06-09 21:57:48 +0930

    First working version of static site

--- Doxie/aurelia-app/app.js
+++ Doxie/aurelia-app/app.js
@@ -4,10 +4,10 @@ import { PLATFORM } from 'aurelia-pal';
 
 export class App {
     async configureRouter(config, router) {
-        config.title = 'Aurelia';
+        config.title = 'Doxie Help Generator';
 		config.map([
 			{ moduleId: PLATFORM.moduleName('/aurelia-app/index'), route: ['', 'index'], name: 'index', nav: true, title: 'Home' },
-			{ moduleId: PLATFORM.moduleName('/aurelia-app/flickr'), route: 'flickr', name: 'flickr', nav: true, title: 'Flickr' }
+			{ moduleId: PLATFORM.moduleName('/aurelia-app/assembly'), route: 'assembly/:id', name: 'assembly', title: 'Assembly' }
 		]);
         this.router = router;
     }
--- Doxie/aurelia-app/index.js
+++ Doxie/aurelia-app/index.js
@@ -1,34 +1,21 @@
-﻿//import {computedFrom} from 'aurelia-framework';
+﻿export class Index {
+    assemblies = [];
 
-export class Index {
-    heading = 'Welcome to the Aurelia Navigation App!';
-    firstName = 'John';
-    lastName = 'Doe';
-    previousValue = this.fullName;
+    constructor() {
+        let self = this;
 
-    //Getters can't be directly observed, so they must be dirty checked.
-    //However, if you tell Aurelia the dependencies, it no longer needs to dirty check the property.
-    //To optimize by declaring the properties that this getter is computed from, uncomment the line below
-    //as well as the corresponding import above.
-    //@computedFrom('firstName', 'lastName')
-    get fullName() {
-        return `${this.firstName} ${this.lastName}`;
+        $.ajax({
+            dataType: "json",
+            url: "/js/assemblies.json",
+            async: false,
+            success: function (json) {
+                $.each(json, function (i) {
+                    self.assemblies.push(this);
+                });
+            }
+        });
     }
 
-    submit() {
-        this.previousValue = this.fullName;
-        alert(`Welcome, ${this.fullName}!`);
-    }
-
-    canDeactivate() {
-        if (this.fullName !== this.previousValue) {
-            return confirm('Are you sure you want to leave?');
-        }
-    }
-}
-
-export class UpperValueConverter {
-    toView(value) {
-        return value && value.toUpperCase();
+    attached() {
     }
 }
