f9417a03a4a07118d253386760bcb557af9402d0
vnmatt <gordon_matt@hotmail.com>
2018-06-09 21:36:19 +0930

    Added new static website project with basic Aurelia template

---
+++ Doxie/aurelia-app/app.js
@@ -0,0 +1,14 @@
+﻿import 'jquery';
+import { HttpClient } from 'aurelia-http-client';
+import { PLATFORM } from 'aurelia-pal';
+
+export class App {
+    async configureRouter(config, router) {
+        config.title = 'Aurelia';
+		config.map([
+			{ moduleId: PLATFORM.moduleName('/aurelia-app/index'), route: ['', 'index'], name: 'index', nav: true, title: 'Home' },
+			{ moduleId: PLATFORM.moduleName('/aurelia-app/flickr'), route: 'flickr', name: 'flickr', nav: true, title: 'Flickr' }
+		]);
+        this.router = router;
+    }
+}
