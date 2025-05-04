eb234c409799620156a2ded7b45dde4c8253504f
Matt <gordon-matt@hotmail.com>
2025-05-04 15:52:23 +0930

    Some work on fixing "Could not load file or assembly.." bug because of framework assemblies not being found.

---
+++ Doxie.Core/ReflectionHelper.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+
+namespace Doxie.Core;
+
+public static class ReflectionHelper
+{
+    public static string GetMethodSignature(MethodInfo methodInfo)
+    {
+        string methodName = methodInfo.Name;
+        string[] parameters = methodInfo.GetParameters()
+            .Select(p => p.ParameterType.Name)
+            .ToArray();
+
+        return $"{methodName}({string.Join(", ", parameters)})";
+    }
+
+    public static string GetPropertySignature(PropertyInfo propertyInfo)
+    {
+        string propertyName = propertyInfo.Name;
+        string propertyType = propertyInfo.PropertyType.Name;
+        return $"{propertyType} {propertyName}";
+    }
+
+    public static string GetConstructorSignature(ConstructorInfo constructorInfo)
+    {
+        string declaringType = constructorInfo.ReflectedType?.Name ?? constructorInfo.DeclaringType?.Name;
+        string[] parameters = constructorInfo.GetParameters()
+            .Select(p => p.ParameterType.Name)
+            .ToArray();
+
+        return $"{declaringType}({string.Join(", ", parameters)})";
+    }
+}
